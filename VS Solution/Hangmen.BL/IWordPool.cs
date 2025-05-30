68c6a15a1bc91cd6f09304fe809e5509a6de66b4
Danylo Borozdin <borozdindanil06@gmail.com>
2025-05-30 18:25:28 +0200

    Add Hangmen.BL project for game logic and word management
    This commit introduces a new project called `Hangmen.BL`, which encapsulates the business logic for the Hangman game. It includes the `IWordPool` interface and the `WordPool` class, providing methods for managing a pool of words. The `Hangman.csproj` and `Hangmen.Console.csproj` files have been updated to reference this new project, enabling the console application to utilize the word pool functionality. The `Program.cs` file has also been updated to demonstrate the usage of the `IWordPool` interface. The `Hangmen.BL.csproj` file has been created with the necessary SDK and properties for targeting .NET 9.0.

---
+++ VS Solution/Hangmen.BL/IWordPool.cs
@@ -0,0 +1,10 @@
+﻿namespace Hangmen.BL;
+
+public interface IWordPool
+{
+    public int GetRandomWord();
+    public string GetWordFromIndex(int index);
+    public string MaskWordFromIndex(int index, int ratio);
+    public string RevealLetter(string maskedWord, int index, char input);
+    public bool ContainsLetter(int index, char letter);
+}
---
+++ VS Solution/Hangmen.BL/WordPool.cs
@@ -0,0 +1,99 @@
+﻿namespace Hangmen.BL;
+
+public sealed class WordPool: IWordPool
+{
+
+    private string[] _words = 
+    {
+        "Apfel",
+        "Hund",
+        "Blume",
+        "Tisch",
+        "Fenster",
+        "Stuhl",
+        "Buch",
+        "Wasser",
+        "Haus",
+        "Schule",
+        "Freund",
+        "Katze",
+        "Baum",
+        "Straße",
+        "Auto",
+        "Wolke",
+        "Zug",
+        "Brot",
+        "Lampe",
+        "Uhr"
+    };
+
+
+    private Random _random = new Random();
+
+    public string GetWordFromIndex(int index)
+    {
+        if(index<0 || index >= _words.Length)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index cannot be negative or bigger than list size");
+
+        return _words[index];
+    }
+    public string MaskWordFromIndex(int index, int ratio)
+    {
+        if (index < 0 || index >= _words.Length)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index cannot be negative or bigger than list size");
+
+        if (ratio <= 0 || ratio >= 100)
+            throw new ArgumentOutOfRangeException(nameof(ratio), "Ratio cannot be lower or equal zero or bigger than 100");
+
+
+        string originalWord = _words[index];
+        char[] maskedChars = originalWord.ToCharArray();
+
+        int wordLength = originalWord.Length;
+        int numberOfLettersToMask = (int)Math.Round(wordLength * (ratio / 100.0));
+
+        // Випадковий вибір позицій для маскування
+        IEnumerable<int> positionsToMask = Enumerable
+            .Range(0, wordLength)
+            .OrderBy(_ => _random.Next())
+            .Take(numberOfLettersToMask);
+
+        foreach (int position in positionsToMask)
+        {
+            maskedChars[position] = '_';
+        }
+
+        return new string(maskedChars);
+    }
+    public int GetRandomWord()
+    {
+        return _random.Next(_words.Length);
+    }
+    public string RevealLetter(string maskedWord, int index, char input)
+    {
+        if (index < 0 || index >= _words.Length)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index cannot be negative or bigger than list size");
+
+        if (maskedWord.Length != _words[index].Length)
+            throw new ArgumentException("Words must be the same length.");
+
+        char[] revealedChars = maskedWord.ToCharArray();
+
+        for (int i = 0; i < _words[index].Length; i++)
+        {
+            if (_words[index][i] == input && maskedWord[i] == '_')
+            {
+                revealedChars[i] = input;
+            }
+        }
+
+        return new string(revealedChars);
+    }
+    public bool ContainsLetter(int index, char letter)
+    {
+        if(index<0 || index >= _words.Length)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index cannot be negative or bigger than list size");
+
+        return _words[index].Contains(letter);
+    }
+}
