85b823e7f01255cd549619875e27e69e5818ab37
Danylo Borozdin <borozdindanil06@gmail.com>
2025-06-04 09:29:47 +0200

    Add letter validation in MainPage.xaml.cs
    Implemented CheckLetter method to validate user input.
    Added using directives for Hangmen.BL.Interfaces and
    Hangmen.BL.Implementation. Updated LetterInput text color
    based on input validity.

--- VS Solution/Hangman/MainPage.xaml.cs
+++ VS Solution/Hangman/MainPage.xaml.cs
@@ -1,15 +1,32 @@
-﻿namespace Hangman
+﻿using Hangmen.BL.Interfaces;
+using Hangmen.BL.Implementation;
+
+namespace Hangman
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void CheckLetter(object? sender, EventArgs e) { }
+        private void CheckLetter(object? sender, EventArgs e)
+        {
+            string? letter = LetterInput.Text;
+
+            if (letter == null)
+            {
+                return;
+            }
+            if (letter.Length != 1 || !char.IsLetter(letter[0]))
+            {
+                LetterInput.TextColor = Colors.Red;
+            }
+            else
+            {
+                LetterInput.TextColor = Colors.Black;
+            }
+        }
         private void EnterLetter(object? sender, EventArgs e) { }
         private void ResetGame(object? sender, EventArgs e) { }
         private void ExitGame(object? sender, EventArgs e) { }
