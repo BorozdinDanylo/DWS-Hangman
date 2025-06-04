eaeb98d8ac9fca1bffecff1201ba09fc95dcbf36
Danylo Borozdin <borozdindanil06@gmail.com>
2025-06-04 19:14:42 +0200

    Add UI enhancements and game logic improvements
    Updated `Hangman.csproj` to include `CommunityToolkit.Maui` and modified package versions. Enhanced `MainPage.xaml` with new UI elements, including a title label, start button, and a grid for wrong letters. Significant changes in `MainPage.xaml.cs` introduced a new game manager interface and improved event handling for game states and user inputs.
    
    Modified `MauiProgram.cs` for application builder setup and updated `Package.appxmanifest` for new capabilities. Cleaned up by deleting unused image files and improved word masking logic in `WordPool.cs`. Adjusted `Program.cs` for game manager instantiation and logging. Added new binary image files for updated visual assets.

--- VS Solution/Hangman/MauiProgram.cs
+++ VS Solution/Hangman/MauiProgram.cs
@@ -1,25 +1,34 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Hangmen.BL.Implementation;
+using Hangmen.BL.Interfaces;
+using Microsoft.Extensions.Logging;
 
-namespace Hangman
+namespace Hangman;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit(options =>
+            {
+                options.SetShouldEnableSnackbarOnWindows(true);
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        builder.Services.AddSingleton<IWordPool, WordPool>();
+        builder.Services.AddSingleton<IGameManager, GameManager>();
+
+        return builder.Build();
     }
 }
